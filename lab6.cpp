680bc12886559df9ce9f6aa287985dbead91e5ba
BabichIKM725v <Kyrylo.Babich@infiz.khpi.edu.ua>
2025-11-28 20:36:08 +0200

    Add files via upload

---
+++ lab6.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    long long dobutock = 1;
+    int i = 1;             
+
+    if (n <= 0) {
+        cout << "Bud laska, vvedit chislo bilshe 0)." << endl;
+        return 1;
+    }
+
+    do {
+        if (n % i == 0) {
+            dobutock = dobutock * i;
+        }
+
+        i++;
+
+    } while (i <= n);
+
+    cout << "Dobutok vsikh dilnykiv chysla " << n << " = " << dobutock << endl;
+
+    return 0;
+}
